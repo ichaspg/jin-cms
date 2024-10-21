8d600bce3eb5584d87ed32a6834dfc0c0f3942c5
ichaspg <ichaspurnama12@gmail.com>
2024-10-21 14:56:59 +0700

    fix: add cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -12,6 +12,7 @@ module.exports = [
         "http://172.168.1.59",
         "http://192.168.100.116:5173",
         "http://172.19.0.1:5173",
+        "https://jin-compro-fe.vercel.app",
       ],
     },
   },
